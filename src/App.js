7e84b8bcff3e21e62eda154b54c5bc0c723a4c91
UmangDalvadi <umangdalvadi8@gmail.com>
2023-03-12 12:59:39 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>UMANG</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vitae veniam culpa totam blanditiis incidunt debitis laudantium consequatur animi, est velit quae dicta ullam quod eaque omnis ipsum expedita commodi. Minima, nemo?</p>
+    
+    </>
   );
 }
 
