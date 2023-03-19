fb2574fd08e93d731fece3873d8baad17235a58a
UmangDalvadi <umangdalvadi8@gmail.com>
2023-03-20 04:41:49 +0530

    add

--- Text-Utiles/src/App.js
+++ Text-Utiles/src/App.js
@@ -4,9 +4,18 @@ import './App.css';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+import About from './components/About';
 
 
 function App() {
+
   const [mode, changemode] = useState('light')
 
   const modefun = () => {
@@ -37,9 +46,24 @@ function App() {
   return (
     < >
 
-      <Navbar title="Textutils" mode={mode} modefun={modefun} />
       <Alert alert={alert} />
-      <Textarea mode={mode} showalert={showalert}/>
+      <Router>
+
+        <Navbar title="Textutils" mode={mode} modefun={modefun} />
+
+        <Routes>
+
+          <Route exact path="/about" element={<About />} />
+          {/* <About />
+          </Route> */}
+          <Route exact path="/" element={<Textarea mode={mode} showalert={showalert} />} />
+          {/* <Textarea mode={mode} showalert={showalert} />
+          </Route> */}
+
+        </Routes>
+
+      </Router>
+
     </>
   );
 }
--- Text-Utiles/src/components/Navbar.js
+++ Text-Utiles/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 // import PropTypes from 'prop-types'
 
 const Navbar = props => {
     return (
         <div>
             <nav className={`navbar navbar-expand-lg navbar navbar-${props.mode} bg-${props.mode}`}>
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <Link className="navbar-brand" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -13,12 +14,12 @@ const Navbar = props => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Link</a>
+                            <Link className="nav-link" to="/about">About</Link>
                         </li>
-                        <li className="nav-item dropdown">
+                        {/* <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Dropdown
                             </a>
@@ -31,15 +32,15 @@ const Navbar = props => {
                         </li>
                         <li className="nav-item">
                             <a className="nav-link disabled" href="/">Disabled</a>
-                        </li>
+                        </li> */}
                     </ul>
                     {/* <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form> */}
                     <div className="form-check form-switch">
-                        <input className="form-check-input" onClick={props.modefun} type="checkbox" id="flexSwitchCheckDefault"/>
-                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
+                        <input className="form-check-input" onClick={props.modefun} type="checkbox" id="flexSwitchCheckDefault" />
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
                     </div>
 
                 </div>
